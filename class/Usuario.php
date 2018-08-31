9ffdfeee60f5104f908d9634168def466fa4329a
mateus <mateus@C-05>
2018-08-31 14:42:57 -0300

    3ª aula - Trabalhando com List

--- class/Usuario.php
+++ class/Usuario.php
@@ -2,7 +2,6 @@
 
 /**
  * Description of Usuario
- *
  * @author mateus
  */
 class Usuario {
@@ -44,8 +43,9 @@ class Usuario {
         $this->dtcadastro = $value;
     }
     
+    //Método que retorna apenas um usuário cadastrado no BD
     public function loadById($id){
-        
+        //estânciando a class Sql()
         $sql = new Sql();
         
         $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", 
@@ -64,13 +64,50 @@ class Usuario {
         }
     }
     
+    //Método que retorna uma lista com todos os usuários cadastrados no BD
+    public static function getList(){
+        //estânciando a class Sql()
+        $sql = new Sql();
+        
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+    }
+    
+    //Método para procurar usuário(s) que contenha $login
+    public static function search($login){
+      $sql = new Sql();
+      return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%".$login."%"));
+    }
+    
+    //Método que retorna um usuário pelo login e senha
+    public function login($login, $password){
+        $sql = new Sql();
+        
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+        ));
+        
+        if(count($results) > 0):
+            $row = $results[0];
+            
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            
+            else:
+                throw new Exception("Login e/ou Senha Inválido(s)!");
+        endif;
+    }
+
+
     public function __toString() {
         
         return json_encode(array(
             "idusuario"=>$this->getIdusuario(),
             "deslogin"=> $this->getDeslogin(),
             "dessenha"=> $this->getDessenha(),
-            "dtcadastro"=> $this->getDtcadastro("d/m/y M:i:s")
+            "dtcadastro"=> $this->getDtcadastro()->format("d/m/Y H:i:s")
         ));
     }
 }
--- index.php
+++ index.php
@@ -8,23 +8,46 @@
     <body>
         <?php
            require_once ("config.php"); 
-           
            /*
-           //Conexão class Sql()
+           //Conexão class Sql(): com select simples
            $sql = new Sql();
            
            $usuarios = $sql->select("SELECT * FROM tb_usuarios");
            
            echo json_encode($usuarios);
-            * 
-            */
+           */ 
            
-           //Conexão class Usuario()
+           /*
+           //Conexão class Usuario(): Consultando o BD por id chamando o método loadById()
            $root = new Usuario();
            $root->loadById(3);
            
            echo $root;
-                   
+           */
+           
+           /*
+           //Conexão class Usuario(): Consultando uma lista de usuários com todos os usuários cadastrados no BD
+           //Como o método class Usuario() ŕ do tipo static, não é preciso estânciá-lo
+           $lista = Usuario::getList();
+           
+           echo json_encode($lista);
+           */
+           
+           /*
+           //carrega um lista de usuarios, buscando pelo $login
+           $search = Usuario::search("Jo");
+           
+           echo json_encode($search);
+           */
+           
+           //carrega um usuário usando o login e a senha
+           $usuario = new Usuario();
+           
+           $usuario->login("root", "!@#$%");
+           
+           echo $usuario;
+           
+           
         ?>
     </body>
 </html>
